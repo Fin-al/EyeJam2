6fbc42c48c1a4f053b44b7ed524f1b2fa2e91ea0
Fin-al <finaltbm@gmail.com>
2026-04-22 01:52:39 -0700

    CODE

--- Assets/scripts/gameplayScripts/collectibleCount.cs
+++ Assets/scripts/gameplayScripts/collectibleCount.cs
@@ -3,17 +3,18 @@ using UnityEngine;
 public class collectibleCount : MonoBehaviour
 {
     int count;
-
+    TMPro.TMP_Text text;
     private void Awake()
     {
-        
+        text = GetComponent<TMPro.TMP_Text>();
     }
     void OnEnable() => collectible.OnCollected += OnCollectibleCollected;
     void OnDisable() => collectible.OnCollected -= OnCollectibleCollected;
 
     void OnCollectibleCollected()
     {
-        Debug.Log(++count);
+        count++;
+        text.text = $"{count}";
     }
     public int getCount()
     {
