8e414545261bb1b39c0126fd8ed34283021a7519
Fin-al <finaltbm@gmail.com>
2026-05-28 01:22:22 -0700

    out of bounds decay

--- Assets/scripts/gameplayScripts/OutofBounds.cs
+++ Assets/scripts/gameplayScripts/OutofBounds.cs
@@ -3,15 +3,48 @@ using UnityEngine;
 public class OutofBounds : MonoBehaviour
 {
     public bool touching = false;
-
+    private int count = 0;
     private void OnTriggerEnter(Collider other)
     {
+        
         if (other.gameObject.CompareTag("Out") && !touching)
         {
-            FarmerTalking.instance.talking("PLAYER: I SHOULD GO BACK.");
-            FarmerTalking.instance.ShowText();
-            Debug.Log("Hit wall");
-            touching = true;
+            count++;
+            switch (count) {  
+                     
+                case 1 or 2:
+                    {
+                        FarmerTalking.instance.talking("PLAYER: I SHOULD GO BACK.");
+                        FarmerTalking.instance.ShowText();
+                        Debug.Log(count);
+                        touching = true;
+                        break;
+                    }
+               
+                case 3 or 4 or 5 or 6 or 7:
+                    {
+                        FarmerTalking.instance.talking("PLAYER: I REALLY NEED TO GO BACK.");
+                        FarmerTalking.instance.ShowText();
+                        Debug.Log("Hit wall");
+                        touching = true;
+                        break;
+                    }
+                case 9:
+                    {
+                        FarmerTalking.instance.talking("MAULDER: GO BACK!");
+                        FarmerTalking.instance.ShowText();
+                         
+                        touching = true;
+                        break;
+                    }
+                    default:
+                    {
+                        FarmerTalking.instance.talking("PLAYER: YOU HEARD HIM!");
+                        FarmerTalking.instance.ShowText();
+                        touching = true;
+                        break;
+                    }
+            }
         }
     }
 
